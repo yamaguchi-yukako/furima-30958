62a43b1fe6249018496497f9edbd7891d9958023
yamaguchi-yukako <y_mar@hotmail.co.jp>
2021-02-03 22:09:19 +0900

    パスワードは、半角英数字混合での入力が必須であること

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
+  resources :items, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
+    
+  end
+  resources :users, only: :show
+
 end
--- db/migrate/20210126111616_devise_create_users.rb
+++ db/migrate/20210126111616_devise_create_users.rb
@@ -6,12 +6,12 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :email,              null: false, unique: true
       t.string :encrypted_password, null: false, default: ""
-      t.string :nickname,null: false
+      t.string :nickname, null: false
+      t.string :last_name, null: false
       t.string :first_name, null: false
-      t.string :family_name, null: false
+      t.string :last_name_kana, null: false
       t.string :first_name_kana, null: false
-      t.string :family_name_kana, null: false
-      t.date :birthday,             null: false
+      t.date :birth_date,             null: false
 
       ## Recoverable
       t.string   :reset_password_token
