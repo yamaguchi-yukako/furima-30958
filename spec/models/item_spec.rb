130a06443b211da532fd83ec643a41e72ff4ffe8
yamaguchi-yukako <y_mar@hotmail.co.jp>
2021-02-22 21:13:41 +0900

    コメントアウト削除、createアクションの追加等

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -21,12 +21,12 @@ RSpec.describe Item, type: :model do
           expect(@item.errors.full_messages).to include("Name can't be blank")
       end
       it '販売価格が299円以下では登録できない' do
-        @item.price = '299'
+        @item.price = 299
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be greater than 299")
       end
       it '販売価格が10000000円以上では登録できない' do
-        @item.price = '10000000'
+        @item.price = 10000000
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be less than 10000000")
       end
@@ -41,27 +41,27 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Info can't be blank")
       end
       it 'カテゴリーが空では登録できない' do  
-        @item.category_id = '0' 
+        @item.category_id = 0
         @item.valid?
         expect(@item.errors.full_messages).to include("Category must be other than 0")
       end
       it '商品の状態が空では登録できない' do  
-        @item.condition_id = '0' 
+        @item.condition_id = 0 
         @item.valid?
         expect(@item.errors.full_messages).to include("Condition must be other than 0")
       end
       it '発送料の負担が空では登録できない' do  
-        @item.cost_id = '0' 
+        @item.cost_id = 0 
         @item.valid?
         expect(@item.errors.full_messages).to include("Cost must be other than 0")
       end
       it '発送元の地域が空では登録できない' do
-        @item.prefecture_code_id = '0'
+        @item.prefecture_code_id = 0
         @item.valid?
         expect(@item.errors.full_messages).to include("Prefecture code must be other than 0")
       end
       it '発送までの日数が空では登録できない' do
-        @item.day_id = '0'
+        @item.day_id = 0
         @item.valid?
         expect(@item.errors.full_messages).to include("Day must be other than 0")
       end
