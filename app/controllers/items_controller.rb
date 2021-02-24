7eca21cfbeae1b8f2d5793a0a4bedd4ff79e2e87
yamaguchi-yukako <y_mar@hotmail.co.jp>
2021-02-24 21:39:20 +0900

    indexアクションの表記を纏める

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!,  only: [:new, :create]
   def index
-    @items = Item.all
-    @items = Item.order("id DESC")
+    @items = Item.all.order(created_at: :desc)
   end
 
   def new
