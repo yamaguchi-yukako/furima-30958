ef3a14b26be5203bbe8a1a857a4a73c2a744ebd2
yamaguchi-yukako <y_mar@hotmail.co.jp>
2021-03-02 15:05:32 +0900

    コードレビュー修正１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -15,9 +15,9 @@ class ItemsController < ApplicationController
       render :new
     end
   end
-  def edit
-    @item = Item.find(params[:id])
-  end
+  #def edit
+   # @item = Item.find(params[:id])
+  #end
   def show
     @item = Item.find(params[:id])
  
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
-  resources :items, only: [:new, :create, :edit, :show] do
+  resources :items, only: [:new, :create, :show] do
   end
 end
