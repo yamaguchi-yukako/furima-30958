8128bd06630b365c3d5576ae5b2c43dabfb33f53
yamaguchi-yukako <y_mar@hotmail.co.jp>
2021-03-03 11:33:18 +0900

    - ログアウト状態のユーザーは、URLを直接入力して商品情報編集ページへ遷移しようとすると、ログインページに遷移すること

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!,  only: [:new, :create]
+  before_action :authenticate_user!,  only: [:new, :create, :edit, :update]
+  before_action :move_to_index, except: [:index, :show]
   def index
     @items = Item.all.order(created_at: :desc)
   end
@@ -17,20 +18,30 @@ class ItemsController < ApplicationController
   end
   def edit
     @item = Item.find(params[:id])
+    
   end
   def update
     @item = Item.find(params[:id])
-    binding.pry
     if @item.update(item_params)
       redirect_to root_path
+    else
+      render :edit
     end
   end
+
   def show
     @item = Item.find(params[:id])
  
   end
+
   private
+
   def item_params
     params.require(:item).permit(:name,:text, :price, :category_id, :cost_id , :condition_id ,:prefecture_code_id, :day_id, :info, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    @item = Item.find(params[:id])
+    redirect_to new_user_session_path unless current_user.id == @item.user_id
   end
 end
