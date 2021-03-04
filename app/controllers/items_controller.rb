641f43440ac67401f10b525b1287ce9b800b9571
yamaguchi-yukako <y_mar@hotmail.co.jp>
2021-03-04 11:02:57 +0900

    rootpathへの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!,  only: [:new, :create, :edit, :update]
+  before_action :authenticate_user!,  only: [:new, :create, :edit, :update, :destroy]
   before_action :set_item, only: [:edit, :show, :update, :destroy]
-  before_action :move_to_index, except: [:index, :show,:new, :create, :destroy]
+  before_action :move_to_index, except: [:index, :show, :new, :create]
  
   def index
     @items = Item.all.order(created_at: :desc)
@@ -47,7 +47,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    redirect_to new_user_session_path unless current_user.id == @item.user_id
+    redirect_to root_path unless current_user.id == @item.user_id
   end
   def set_item
     @item = Item.find(params[:id])
